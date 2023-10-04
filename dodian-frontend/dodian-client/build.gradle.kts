821b899b757cae82089e1adda5d44cf9811ac30f
Nozemi <nozemi@dodian.net>
2023-10-04 18:35:33 +0200

    Simple Client Run Scripts

--- dodian-frontend/dodian-client/build.gradle.kts
+++ dodian-frontend/dodian-client/build.gradle.kts
@@ -1,11 +1,34 @@
-plugins {
-    application
-}
-
 dependencies {
     implementation(project(":dodian-common:dodian-library"))
 
     implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("org.apache.commons:commons-compress:1.21")
     implementation("org.apache.commons:commons-collections4:4.4")
+}
+
+fun JavaExec.configure(
+    host: String = "127.0.0.1",
+    gamePort: Int = 43594,
+    filePort: Int = 43595,
+    httpPort: Int = 8080
+) {
+    group = "dodian-client"
+
+    workingDir = project.projectDir
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("net.dodian.uber.client.GameClientKt")
+    args = listOf(
+        "--host=$host",
+        "--gamePort=$gamePort",
+        "--filePort=$filePort",
+        "--httpPort=$httpPort"
+    )
+}
+
+tasks.register<JavaExec>("localhostRun") {
+    configure()
+}
+
+tasks.register<JavaExec>("liveRun") {
+    configure(host = "play.dodian.net", httpPort = 43596)
 }
