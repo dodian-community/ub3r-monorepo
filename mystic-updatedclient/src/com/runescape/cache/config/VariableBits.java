3e2d481f73af0ecd93482a9e23aa985e6407c379
Neosback <sythril2010@gmail.com>
2025-11-23 19:06:16 -0600

    Fix for varbit mismatch

--- mystic-updatedclient/src/com/runescape/cache/config/VariableBits.java
+++ mystic-updatedclient/src/com/runescape/cache/config/VariableBits.java
@@ -14,35 +14,42 @@ public final class VariableBits {
 	public static void init(FileArchive streamLoader) {
 		Buffer stream = new Buffer(streamLoader.readFile("varbit.dat"));
 		int size = stream.readUShort();
-		
+
 		if (varbits == null) {
 			varbits = new VariableBits[size];
 		}
-		
+
 		for (int index = 0; index < size; index++) {
-			
+
 			if (varbits[index] == null) {
 				varbits[index] = new VariableBits();
 			}
-			
+
 			varbits[index].decode(stream);
-			
+
 			if (varbits[index].aBoolean651) {
 				VariablePlayer.variables[varbits[index].setting].aBoolean713 = true;
 			}
-			
+
 		}
 
 		if (stream.currentPosition != stream.payload.length) {
 			System.out.println("varbit load mismatch");
 		}
-		
+
 	}
 
 	private void decode(Buffer stream) {
-		setting = stream.readUShort();
-		low = stream.readUnsignedByte();
-		high = stream.readUnsignedByte();
+		int opcode = stream.readUnsignedByte();
+		if (opcode == 0) {
+			return;
+		} else if (opcode == 1) {
+			setting = stream.readUShort();
+			low = stream.readUnsignedByte();
+			high = stream.readUnsignedByte();
+		} else {
+			System.out.println("[VARBIT WARNING] Unknown opcode: " + opcode);
+		}
 	}
 
 	private VariableBits() {
