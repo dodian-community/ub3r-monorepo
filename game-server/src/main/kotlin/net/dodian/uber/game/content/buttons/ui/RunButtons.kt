78b31ec610721d2b7a03df4675b6e1bba2ac75ee
Neosback <sythril2010@gmail.com>
2026-02-07 20:54:51 -0600

    Fix button handling on network layer

--- game-server/src/main/kotlin/net/dodian/uber/game/content/buttons/ui/RunButtons.kt
+++ game-server/src/main/kotlin/net/dodian/uber/game/content/buttons/ui/RunButtons.kt
@@ -4,12 +4,12 @@ import net.dodian.uber.game.content.buttons.ButtonContent
 import net.dodian.uber.game.model.entity.player.Client
 
 object RunButtons : ButtonContent {
-    override val buttonIds: IntArray = intArrayOf(152, 153, 74214)
+    override val buttonIds: IntArray = intArrayOf(152, 19158, 74214)
 
     override fun onClick(client: Client, buttonId: Int): Boolean {
         when (buttonId) {
             152 -> client.buttonOnRun = false
-            153 -> client.buttonOnRun = true
+            19158 -> client.buttonOnRun = true
             74214 -> client.buttonOnRun = !client.buttonOnRun
             else -> return false
         }
