aea1f86da9d2341328c99186192c0181d575692a
Neosback <sythril2010@gmail.com>
2026-03-27 04:10:10 -0500

    cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,6 @@
 rootProject.name = "ub3r"
 
 include(":game-server")
-include(":game-plugin-index-annotations")
-include(":game-plugin-index-processor")
 include(":game-client")
 include(":mystic-updatedclient")
 include(":game-launcher")
